19584a0b13f8d8eff05588f76b2eb779eae3f7f7
GlenonGiii <glenon.studio@gmail.com>
2021-04-05 20:40:12 +0700

    update

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-(function ($) {
-    'use strict'
+'use strict'
 
-    const navbar = document.getElementById("navbar");
-    const btntop = document.getElementById("btntop");
+const navbar = document.getElementById("navbar");
+const btntop = document.getElementById("btntop");
 
-    window.addEventListener("scroll", function () {
-      navbar.classList.toggle("navbar-scroll", window.scrollY > 100);
-      btntop.classList.toggle("scroll", window.scrollY > 500);
-    });
+window.addEventListener("scroll", function () {
+  navbar.classList.toggle("navbar-scroll", window.scrollY > 100);
+  btntop.classList.toggle("scroll", window.scrollY > 500);
+});
 
-    btntop.addEventListener("click", function (e) {
-      e.preventDefault();
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    });
-})(jQuery)
+btntop.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
